40dfdf7da7f9db65a948418b5e08143d5e5c3af0
Ravi Nain <91778452+ravinainn@users.noreply.github.com>
2023-06-30 12:19:50 +0530

    Add files via upload

--- js/courses.js
+++ js/courses.js
@@ -3,40 +3,85 @@ const description = document.querySelector('.description');
 
 const descriptionContent = [
     
-        ` <h2>Python Programming (Includes Game development)</h2>
-        <p>This is a four-week interactive training programme that consists of three classes per week. You'll learn Python programming, data structures, advanced libraries, and game development. There is an optional project with mentorship, and based on performance, an internship interview may be offered. At the end of the programme, you will also receive an eCertificate from AI-Shala.
+    ` <h2>Python for Data Science</h2>
+        <p>
+            
+    This program imparts machine learning knowledge, covering model learning, evaluation, and algorithms. Over 4 weeks with 3 lectures/week, participants learn problem-solving, receive interview guidance, and earn an eCertificate. Top performers can extend the program for an AI project.
         </p>
         <h3>Training program benefits:</h3>
         <ol class="course_benefits">
-            <li class="li1">
+            <li>
+            Knowledge in machine learning
+    
+            </li>
+            <li>
+            Interactive and engaging training
+    
+            </li>
+            <li>
+            Practical problem-solving skills
+    
+            </li>
+            <li>
+            Interview guidance for job readiness
+    
+            </li>
+            <li>
+            Opportunity for project extension
+    
+            </li>
+            <li>
+            eCertificate upon program completion
+    
+            </li>
+            
+        </ol>
+    
+        <div class="sec_link">
+            <a href="./Python-for-data-science/index.html">Read more...</a>
+        </div>
+    
+        <a href='blank' target="_blank">
+            <button class="register">
+                Register Now!
+            </button>
+        </a> `,
+
+    ` <h2>Python Programming (Includes Game development)</h2>
+        <p>
+            The 4-week training program includes 3 classes/week for Python, data structures, advanced libraries, and game development. Participants may opt for a mentorship project and could be offered an internship interview based on performance. An eCertificate will be awarded upon completion.
+        </p>
+        <h3>Training program benefits:</h3>
+        <ol class="course_benefits">
+            <li>
                 Python programming live lectures (on the spot doubt clearance)
 
             </li>
-            <li class="li2">
+            <li>
                 Basic and advanced python libraries
 
             </li>
-            <li class="li3">
+            <li>
                 Game development lectures
 
             </li>
-            <li class="li4">
+            <li>
                 Quizzes for Self assessment
 
             </li>
-            <li class="li5">
+            <li>
                 Live lectures with coding
 
             </li>
-            <li class="li6">
+            <li>
                 Project for performance evaluation (optional)
 
             </li>
-            <li class="li7">
+            <li>
                 Opportunity for internship with AI-Shala
 
             </li>
-            <li class="li8">
+            <li>
                 Certificate program
 
             </li>
@@ -49,40 +94,42 @@ const descriptionContent = [
         </a> `,
 
 
-        ` <h2>Dive in to Machine Learning Bootcamp</h2>
-        <p>With lectures using the flipped classroom learning paradigm and interview coaching, participants in this three-month machine learning training programme will learn how to approach and solve problems using machine learning. On the basis of their performance, they will finish a project of their choice, present, and have the chance to interview for an internship with one of our hiring partners. Upon programme completion, AI-Shala will provide an eCertificate to each participant.</p>
+
+
+    ` <h2>Dive in to Machine Learning Bootcamp</h2>
+        <p>The 3-month machine learning training program uses flipped classroom learning and interview coaching. Participants will learn problem-solving using machine learning and finish a project, with the possibility of interviewing for an internship with a hiring partner. An eCertificate will be provided by AI-Shala upon completion.</p>
         <h3>Training program benefits:</h3>
         <ol class="course_benefits">
-            <li class="li1">
+            <li>
             Learn how to approach a problem using Machine Learning
 
             </li>
             
-            <li class="li2">
+            <li>
             Flipped classroom learning model and live doubt clearance
 
             </li>
-            <li class="li3">
+            <li>
             Mentorship for a startup or the research paper post completion
 
             </li>
-            <li class="li4">
+            <li>
             3/6 month internship opportunity with our hiring partners post completion
 
             </li>
-            <li class="li5">
+            <li>
             Learn how to write a research paper
 
             </li>
-            <li class="li6">
+            <li>
             Includes dedicated two months for a project which can extend to either a research paper or a startup
 
             </li>
-            <li class="li7">
+            <li>
             ML interview preparation from industry experts working in top companies
 
             </li>
-            <li class="li8">
+            <li>
             Certificate program
 
             </li>
@@ -95,39 +142,39 @@ const descriptionContent = [
         </a> `,
 
 
-        ` <h2>Advance Machine Learning</h2>
-        <p>Lectures on machine learning and entrepreneurship are included in this six-week summer training programme, along with advice on how to prepare for interviews, a project that must be completed, and prospective internship and incubator chances. AI-Shala and IIITD-IC will work together to provide participants with an eCertificate, with opportunities based on performance and IIITD-IC policy.</p>
+    ` <h2>Advance Machine Learning</h2>
+        <p>The 6-week summer program offers machine learning and entrepreneurship lectures, interview preparation, a project requirement, internships and incubators opportunities, and an eCertificate from AI-Shala and IIITD-IC. Performance determines eligibility.</p>
         <h3>Training program benefits:</h3>
         <ol class="course_benefits">
-            <li class="li1">
+            <li>
             Machine learning lectures (how to approach and solve problems using ML)
 
             </li>
             
-            <li class="li2">
+            <li>
             Interview guidance
 
             </li>
-            <li class="li3">
+            <li>
             Quizzes for Self assessment
 
             </li>
-            <li class="li4">
+            <li>
             Lectures on entrepreneurship
 
             </li>
-            <li class="li5">
+            <li>
             Project for performance evaluation
 
             </li>
-            <li class="li6">
+            <li>
             Opportunity for internship with AI-Shala hiring partners
             </li>
-            <li class="li7">
+            <li>
             Opportunity for incubation at IIITD-IC
 
             </li>
-            <li class="li8">
+            <li>
             Certificate program jointly from AI-Shala and IIITD-IC
 
             </li>
@@ -139,33 +186,55 @@ const descriptionContent = [
             </button>
         </a> `,
 
-        `
+    `
         <div class="course_soon">
         <button>Coming soon</button>
         </div>`,
-        `
+    `
         <div class="course_soon">
         <button>Coming soon</button>
         </div>`,
-        `
+    `
         <div class="course_soon">
         <button>Coming soon</button>
-        </div>`,
-        `
-        <div class="course_soon">
-        <button>Coming soon</button>
-        </div>`,
+        </div>`
+
 ];
 
+
 listItems.forEach((item, index) => {
-    item.addEventListener('mouseover', () => {
+    item.addEventListener('click', () => {
         listItems.forEach(item => {
             item.classList.remove('active');
         });
         item.classList.add('active');
         description.innerHTML = descriptionContent[index];
-        
+        // description.querySelector('h2').textContent = descriptions[index].mainHeading;
+        // description.querySelector('p').innerHTML = descriptions[index].content;
+        // description.querySelector('h3').textContent = descriptions[index].subHeading;
+        // description.querySelector('.li1').textContent = descriptions[index].benefits[0];
+        // description.querySelector('.li2').textContent = descriptions[index].benefits[1];
+        // description.querySelector('.li3').textContent = descriptions[index].benefits[2];
+        // description.querySelector('.li4').textContent = descriptions[index].benefits[3];
+        // description.querySelector('.li5').textContent = descriptions[index].benefits[4];
+        // description.querySelector('.li6').textContent = descriptions[index].benefits[5];
+        // description.querySelector('.li7').textContent = descriptions[index].benefits[6];
+        // description.querySelector('.li8').textContent = descriptions[index].benefits[7];
+        // description.querySelector('a').href = descriptions[index].btn;
     });
 });
+
 description.innerHTML = descriptionContent[0];
+// description.querySelector('h2').textContent = descriptions[0].mainHeading;
+// description.querySelector('p').innerHTML = descriptions[0].content;
+// description.querySelector('h3').textContent = descriptions[0].subHeading;
+// description.querySelector('.li1').textContent = descriptions[index].benefits[0];
+// description.querySelector('.li2').textContent = descriptions[0].benefits[1];
+// description.querySelector('.li3').textContent = descriptions[0].benefits[2];
+// description.querySelector('.li4').textContent = descriptions[0].benefits[3];
+// description.querySelector('.li5').textContent = descriptions[0].benefits[4];
+// description.querySelector('.li6').textContent = descriptions[0].benefits[5];
+// description.querySelector('.li7').textContent = descriptions[0].benefits[6];
+// description.querySelector('.li8').textContent = descriptions[0].benefits[7];
+// description.querySelector('a').href = descriptions[0].btn;
 listItems[0].classList.add('active');
